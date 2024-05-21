d2cdf2fc8b50897b22ffc0470aee85e0130abd25
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-05-21 12:35:44 -0400

    Add Arrays

--- lecture/02-Fundamentals-Part-2/starter/script.js
+++ lecture/02-Fundamentals-Part-2/starter/script.js
@@ -64,3 +64,48 @@ if (scoreDolphins > scoreKoalas) {
 } else {
     console.log(`koalas win ${scoreKoalas}`);
 }
+
+// intraduction Arrays
+
+const friend1 = "Michele";
+const friend2 = "Peter";
+const friend3 = "John";
+
+const friends = ["Michele", "Peter", "John"];
+console.log(friends);
+
+const years = new Array(1991, 2000, 2005, 2010, 2024);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "jay";
+console.log(friends);
+
+const firstName = "Johnas";
+const johnas = [firstName, "jshiudhjo", 2024 - 1999, "teacher", friends];
+console.log(johnas);
+console.log(johnas.length);
+
+// exersize
+
+const calcAge = function (birthyear) {
+    return 2037 - birthyear;
+};
+const years1 = [1990, 2000, 2010, 2020, 2030];
+
+const age4 = calcAge(years1[0]);
+const age5 = calcAge(years1[1]);
+const age6 = calcAge(years1[years1.length - 1]);
+
+console.log(age4, age5, age6);
+
+const ages = [
+    calcAge(years1[0]),
+    calcAge(years1[1]),
+    calcAge(years1[years1.length - 1])
+];
+console.log(ages);
