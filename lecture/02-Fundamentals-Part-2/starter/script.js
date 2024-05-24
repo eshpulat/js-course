c949f5ad701bdf7d1cbdf62fc9a18bf5fffae3d3
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-05-23 21:03:22 -0400

    finish fundamental JavaScript

--- lecture/02-Fundamentals-Part-2/starter/script.js
+++ lecture/02-Fundamentals-Part-2/starter/script.js
@@ -295,3 +295,19 @@ while (dice !== 6) {
     console.log(`you rolled a ${dice}`);
     dice = Math.trunc(Math.random() * 6) + 1;
 }
+
+// assignment
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < 10; i++) {
+    totals.push(calcTip(bills[i]) + bills[i]);
+}
+console.log(totals);
